0e6c77f1503c94072503939ce580eb24e3da563d
Ben Rush <brush701@gmail.com>
2022-01-19 04:57:45 +0000

    Update for new API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import {
-  MarkdownSourceView,
-  MarkdownView,
   Plugin
 } from 'obsidian';
 
-import * as CodeMirror from 'codemirror'
+
 import * as Y from 'yjs'
 import { WebrtcProvider } from 'y-webrtc'
 import { CodemirrorBinding } from 'y-codemirror'
@@ -15,22 +13,17 @@ export default class MultiplayerPlugin extends Plugin {
   binding: CodemirrorBinding;
   provider: WebrtcProvider;
 
-  get editor(): CodeMirror.Editor {
-    const view = this.app.workspace.activeLeaf.view;
-    if (!(view instanceof MarkdownView)) return null;
-
-    const sourceView = view.sourceMode;
-    return (sourceView as MarkdownSourceView).cmEditor;
-  }
-
   async onload() {
     const ydoc = new Y.Doc()
-    var ytext = ydoc.getText('brush-test');
+    const ytext = ydoc.getText('brush-test');
     this.provider = new WebrtcProvider(
       'brush-test',
       ydoc
     )
-    this.binding = new CodemirrorBinding(ytext, this.editor, this.provider.awareness)
+
+    this.registerCodeMirror(editor => {
+      this.binding = new CodemirrorBinding(ytext, editor, this.provider.awareness)
+      });
     console.log("loaded obsidian-multiplayer")
   }
 
